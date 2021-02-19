e521a772c7197ae4daefc324746bff3ba4d8fdf6
LIFEDISSENTER <59603721+LIFEDISSENTER@users.noreply.github.com>
2021-02-20 02:56:20 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8"><!--KARTIKEY NARAIYAN GARG-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title> Sparks Basic Banking System</title>
+  </head><!--KARTIKEY NARAIYAN GARG-->
+
+  <body>
+	  <?php
+	  include 'navbar.php';
+	  ?>
+	  
+		  <div class="container-fluid">
+	
+				<div class="row intro py-1" style="background:#12a8e0;">
+				  <div class="col-sm-12 col-md">
+					<div class="heading text-center my-5" >
+					  <h3>Welcome to</h3>
+					  <h1>GARG'S BANK</h1><!--KARTIKEY NARAIYAN GARG-->
+					</div>
+				  </div>
+				  <div class="col-sm-12 col-md img text-center">
+					<img src="img/BK1.jpg" class="img-fluid pt-2" width="400px" height="360px">
+				  </div>
+				</div>
+
+
+				<div class="row activity text-center">
+			 
+					  
+					  <div class="col-md  mt-5 act">
+						<img src="img/c3.jpg" class="img-fluid"><!--KARTIKEY NARAIYAN GARG-->
+						<br>
+						<a href="viewall.php"><button style="background:#280071;color:#fff;">View Customers</button></a>
+					  </div>
+					  <div class="col-md  act">
+						<img src="img/m3.jpg" class="img-fluid" width="225px" height="225px">
+						<br>
+						<a href="transfermoney.php"><button style="background:#280071;color:#fff;">Pay Money</button></a>
+					  </div>
+					  <div class="col-md mt-5 act">
+						<img src="img/t3.jpg" class="img-fluid" width="225px" height="225px">
+						<br>
+						<a href="transactionhistory.php"><button style="background:#280071;color:#fff;">Transaction History</button></a>
+					  </div>
+				</div>
+		  </div>
+		  <footer class="text-center mt-4 py-2" style="background:#12a8e0;color:#280071;"><!--KARTIKEY NARAIYAN GARG-->
+			<p>&copy 2021 Developed By: <b>KARTIKEY NARAIYAN GARG</b> <br> The Sparks Foundation</p>
+		  </footer><!--KARTIKEY NARAIYAN GARG-->
+		  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+	</body>
+</html>
